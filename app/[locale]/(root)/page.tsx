b662223db7048e2869a676b6947ac5860cf6079d
MarkupDeveolop <markupdevelopment@gmail.com>
2026-02-24 00:58:22 +0200

    updated

--- app/[locale]/(root)/page.tsx
+++ app/[locale]/(root)/page.tsx
@@ -1,43 +1,38 @@
-import Header from '@/components/Action-Interface/User/common/Header';
-import { generateLocalizedMetadata } from '@/components/Action-Interface/User/common/SEO/generateLocalizedMetadata';
-import AboutSection from '@/components/Action-Interface/User/Sections/About/About';
-import Hero from '@/components/Action-Interface/User/Sections/HeroSection/Hero';
-import ProjectsSection from '@/components/Action-Interface/User/Sections/Projects/ProjectSection';
-import Skills from '@/components/Action-Interface/User/Sections/Skills/Skills';
-
-
+import Header from "@/components/Action-Interface/User/common/Header";
+import { generateLocalizedMetadata } from "@/components/Action-Interface/User/common/SEO/generateLocalizedMetadata";
+import AboutSection from "@/components/Action-Interface/User/Sections/About/About";
+import Hero from "@/components/Action-Interface/User/Sections/HeroSection/Hero";
+import ProjectsSection from "@/components/Action-Interface/User/Sections/Projects/ProjectSection";
+import Skills from "@/components/Action-Interface/User/Sections/Skills/Skills";
 
 export const generateMetadata = async () =>
   generateLocalizedMetadata({
-    titleEn: "Let’s Build Your Next Web Project Together",
-titleAr: "دعنا نبني مشروعك الرقمي القادم معًا",
+    titleEn: "HG-Development",
+    titleAr: "HG-Development",
 
-descriptionEn:
-  "Looking for a web developer? Let’s collaborate to build fast, scalable, and modern web applications.",
-descriptionAr:
-  "هل تبحث عن مطور ويب؟ دعنا نتعاون لبناء تطبيقات ويب حديثة وسريعة وقابلة للتوسع.",
+    descriptionEn:
+      "Let’s Build Your Next Web Project Together, Looking for a web developer? Let’s collaborate to build fast, scalable, and modern web applications.",
+    descriptionAr:
+      "دعنا نبني مشروعك الرقمي القادم معًا هل تبحث عن مطور ويب؟ دعنا نتعاون لبناء تطبيقات ويب حديثة وسريعة وقابلة للتوسع. ",
 
     keywordsEn:
-  "hire web developer, full stack developer, freelance web developer, web development services, build web app, website development, modern web applications, next.js developer, react developer, scalable web solutions",
+      "hire web developer, full stack developer, freelance web developer, web development services, build web app, website development, modern web applications, next.js developer, react developer, scalable web solutions",
 
-keywordsAr:
-  "توظيف مطور ويب, مطور ويب متكامل, خدمات تطوير المواقع, إنشاء تطبيق ويب, تصميم مواقع احترافية, تطوير تطبيقات ويب, مطور Next.js, مطور React, حلول ويب قابلة للتوسع",
+    keywordsAr:
+      "توظيف مطور ويب, مطور ويب متكامل, خدمات تطوير المواقع, إنشاء تطبيق ويب, تصميم مواقع احترافية, تطوير تطبيقات ويب, مطور Next.js, مطور React, حلول ويب قابلة للتوسع",
 
     path: "/home",
   });
 
-
 const HomePage = () => {
-
-
   return (
-    <main className=''>
-        <Hero />
-        <AboutSection />
-        <Skills />
-        <ProjectsSection />
+    <main className="">
+      <Hero />
+      <AboutSection />
+      <Skills />
+      <ProjectsSection />
     </main>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
